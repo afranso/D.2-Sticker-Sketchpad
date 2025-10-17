2ab3a36c14a5a45cbf3a1695abfa9d76cf283104
afranso <afranso@ucsc.edu>
2025-10-17 00:29:32 +0000

    Step 1: Added title, canvas, and canvas border
    Co-authored-by: Kalisone <emorelos04@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
+<h1>Sticker Sketchpad</h1>
+  <canvas id="myCanvas" width="256" height="256"></canvas>
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
