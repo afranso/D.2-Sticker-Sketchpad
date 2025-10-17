86aaaf57dd00f9b9ccd65555e93f1d3c3344c2bd
afranso <afranso@ucsc.edu>
2025-10-17 00:49:04 +0000

    Step 2: Added drawing function and clear button

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,57 @@ document.body.innerHTML = `
   <canvas id="myCanvas" width="256" height="256"></canvas>
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+
+const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+const context = canvas.getContext("2d")!;
+
+let x = 0;
+let y = 0;
+let isDrawing = false;
+
+canvas.addEventListener("mousedown", (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  if (isDrawing) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+function drawLine(
+  context: CanvasRenderingContext2D,
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number,
+) {
+  context.beginPath();
+  context.strokeStyle = "black";
+  context.lineWidth = 1;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
+}
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+document.body.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+});
